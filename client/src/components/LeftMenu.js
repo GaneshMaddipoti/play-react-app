dd978d34123cce6c3184eaa614ccb6a0d50e0a93
maddipoti.ganesh <maddipoti.ganesh@gmail.com>
2025-05-11 21:45:58 +0000

    changed layout

---
+++ client/src/components/LeftMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './LeftMenu.css';
+function LeftMenu() {
+  return (
+    <div className="left-menu">
+      <h2>Navigation</h2>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default LeftMenu;
---
+++ client/src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainContent.css';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <h2>Main Content Area</h2>
+      <p>This is the main content of the application.</p>
+      {/* Add your main content here */}
+    </div>
+  );
+}
+
+export default MainContent;
